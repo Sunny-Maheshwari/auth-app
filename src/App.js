8b99ba37e42bf8203960b43226f3883ba18637b2
SunnyD125 <maheshwarisunny17@mail.com>
2021-12-22 01:21:57 -0800

    my 1st comit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import LoginButton from './Components/LoginButton';
+
+const App = () => {
+  return (
+    <>
+      <LoginButton />
+    </>
+  )
+}
+
+export default App;
